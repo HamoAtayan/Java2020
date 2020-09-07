af622f9663453ae7648fab4ab60fd16c5d8d120d
Hmayak Atayan <hmayak.atayan@webbfontaine.com>
2020-09-07 08:30:48 +0400

    first commit

---
+++ src/main/java/com/datatypes/Print.java
@@ -0,0 +1,10 @@
+package com.datatypes;
+
+/**
+ * Created by Hmayak Atayan on  07, Sep, 2020
+ */
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("ping");
+    }
+}
